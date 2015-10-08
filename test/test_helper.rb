68ad0d09e265180e12eda151ee0bbbf50857b327
Al Snow <jasnow@hotmail.com>
2015-10-08 07:56:43 -0400

    Fixed the rest of rubocop msgs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,12 @@ require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in
-  #    alphabetical order.
-  fixtures :all
+class ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in
+    #    alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
+  end
 end
